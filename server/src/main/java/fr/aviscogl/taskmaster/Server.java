9457651d0a0c0ef39f4ff5f0974395378f75aa5a
alexisvisco <alexis.viscogliosi@outlook.fr>
2018-03-13 20:24:44 +0100

    add .idea to work at 101

--- server/src/main/java/fr/aviscogl/taskmaster/Server.java
+++ server/src/main/java/fr/aviscogl/taskmaster/Server.java
@@ -3,10 +3,7 @@ package fr.aviscogl.taskmaster;
 import fr.aviscogl.taskmaster.log.Logger;
 import fr.aviscogl.taskmaster.manage.RequestHandler;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -15,6 +12,7 @@ public class Server {
 
     public static int PORT = 9898;
     public static int CLIENT_NUMBER = 0;
+
 
     public static void main(String[] args) throws Exception {
         System.out.println("The capitalization server is running.");
