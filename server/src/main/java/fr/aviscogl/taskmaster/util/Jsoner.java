33f25417eabae508da0b8e6648d4fcc10c56c953
AlexisVisco <kwizzy@outlook.fr>
2018-03-27 17:50:10 +0200

    update jsoner

--- server/src/main/java/fr/aviscogl/taskmaster/util/Jsoner.java
+++ server/src/main/java/fr/aviscogl/taskmaster/util/Jsoner.java
@@ -3,24 +3,21 @@ package fr.aviscogl.taskmaster.util;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
-import org.json.JSONObject;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.Optional;
 
 public class Jsoner {
-    static Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+
+    private static Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
 
     public static <T> Optional<T> getJsonFromFile(File f, Class<T> cl) {
         try {
             JsonReader reader = new JsonReader(new FileReader(f));
-            return Optional.ofNullable(gson.fromJson(reader, cl));
+            return Optional.ofNullable(GSON.fromJson(reader, cl));
         } catch (IOException e) {
             e.printStackTrace();
         }
         return Optional.empty();
     }
-
 }
